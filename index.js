9f39df0af3ad9c05bcb7e768fe2df50eda4d87e3
7upesi <7upesi@gmail.com>
2018-05-23 14:09:05 -0400

    Does a ping pong

--- index.js
+++ index.js
@@ -6,3 +6,12 @@ client.on('ready', () => {
 });
 
 client.login('your-token-goes-here');
+
+client.on('message', message => {
+    console.log(message.content);
+});
+
+if (message.content === '!ping') {
+    // send back "Pong." to the channel the message was sent in
+    message.channel.send('Pong.');
+}
