7e7cc94a7ad0008f37ba5d8f2862cbb2695cfc76
Aerosaul <abcde560@gmail.com>
2018-05-23 14:36:59 -0400

    Fixed merge for real this time I promise

--- index.js
+++ index.js
@@ -5,26 +5,15 @@ client.on('ready', () => {
     console.log('Ready!');
 });
 
-client.login('your-token-goes-here');
+client.login('NDQ4OTAzMjg5NjUyNzcyODY1.DedDZg.Hstox9kalyoMuZ0IvHEiBTnoo2Y');
 
 client.on('message', message => {
-<<<<<<< HEAD
     if (message.content === '!ping') {
         // send back "Pong." to the channel the message was sent in
         message.channel.send('Pong.');
     }
+    if (message.content === '!pong') {
+        // send back "Pong." to the channel the message was sent in
+        message.channel.send('Ping Ping!');
+    }
 });
-=======
-    console.log(message.content);
-});
-
-if (message.content === '!ping') {
-    // send back "Pong." to the channel the message was sent in
-    message.channel.send('Pong.');
-}
-
-if (message.content === '!pong') {
-    // send back "Pong." to the channel the message was sent in
-    message.channel.send('Ping Ping!');
-}
->>>>>>> c131022d2e31b853fe57c8b051fec5c319f09f1e
