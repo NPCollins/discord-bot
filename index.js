f542f32a641d8056f3e267d7f187ad7a0c83f5ee
7upesi <7upesi@gmail.com>
2018-05-24 23:27:53 -0400

    Revert "brett testo"
    This reverts commit 5522657581a3bc8651e4e3fc5f446ce652f0ea50.

--- index.js
+++ index.js
@@ -6,28 +6,6 @@ client.on('ready', () => {
     console.log('Ready!');
 });
 
-var http = require("http");
-    url = "https://dog.ceo/api/breeds/image/random";
-
-    var request = http.get(url, function (response) {
-        // data is streamed in chunks from the server
-        // so we have to handle the "data" event    
-        var buffer = "", 
-            data,
-            route;
-    
-        response.on("data", function (chunk) {
-            buffer += chunk;
-        }); 
-    
-        response.on("end", function (err) {
-            // finished transferring data
-            // dump the raw data
-            console.log(buffer);
-            console.log("\n");
-            data = JSON.parse(buffer);
-        }); 
-    }); 
 client.login(token);
 
 client.on('message', message => {
@@ -38,10 +16,8 @@ client.on('message', message => {
     if (message.content === `${prefix}server`) {
         message.channel.send(`Server name: ${message.guild.name}\nTotal members: ${message.guild.memberCount}`);
     }
-    if(message.content.startsWith(prefix + 'doggo')) {
-        
-        if(data.status == 'success') {
-            message.channel.send(data.message);
-        }
+    if(message.content.startsWith(prefix + 'meme')) {
+        // things with the memes
+        message.channel.send('Shits not ready yo');
     }
 });
