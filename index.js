14512e0b884c2ab7d56e33081cc3fcdc1a00b77f
7upesi <7upesi@gmail.com>
2018-06-06 21:52:33 -0400

    push2 electric bungaloo

--- index.js
+++ index.js
@@ -13,6 +13,32 @@ client.on('ready', () => {
 
 client.login(token);
 
+function getBuffer(response){
+
+    let buffer = "";
+    var data;
+
+    response.on('data', addChunk);
+    response.on('end', getErr);
+    
+
+}
+
+function getErr(err){
+
+    console.log(buffer);
+    console.log('\n');
+    data = JSON.parse(buffer);
+    if(data.status == 'success') {
+        message.channel.send(data.message);
+    }
+
+}
+
+function addChuck(chunk){
+    buffer += chunk;
+}
+
 client.on('message', message => {
     if (message.content.startsWith(prefix + 'ping')) {
         // send back "Pong." to the channel the message was sent in
@@ -27,24 +53,10 @@ client.on('message', message => {
     }
     if(message.content.startsWith(prefix + 'doggo')) {
 
-        https.get(doggoUrl, function(response) {
-            // data is streamed in chunks from the server
-            // so we have to handle the "data" event
-            let buffer = '',
-                data;
-
-            response.on('data', function(chunk) {
-                buffer += chunk;
-            });
-
-            response.on('end', function(err) {
-                // finished transferring data
-                // dump the raw data
-                console.log(buffer);
-                console.log('\n');
-                data = JSON.parse(buffer);
+        https.get(doggoUrl, addBuffer(response));
 
     }
+
     if(message.content.startsWith(prefix + 'meow')) {
 
         http.get(kittyUrl, function(response) {
@@ -68,4 +80,4 @@ client.on('message', message => {
             });
         });
     }
-});
+}
